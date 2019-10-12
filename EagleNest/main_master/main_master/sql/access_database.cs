09f207826cd1e43dab20f8b245eb18ed8c203141
hecodesfortheusers <jwjoines@eagles.usi.edu>
2019-10-12 01:28:26 -0500

    added access_database class to hide access information, reformated some things, sorry for pushing to master, i forgot

---
+++ EagleNest/main_master/main_master/sql/access_database.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace main_master
+{
+    public class access_database
+    {
+        private string username;
+        private string password;
+        private string server;
+        private string name;
+
+        public access_database() {
+            pull_access_info();
+        }
+
+        private void pull_access_info() {
+                string path = System.Web.HttpContext.Current.Server.MapPath(@"~/sql/access.config");
+                string[] lines = System.IO.File.ReadAllLines(path);
+                
+                    username = lines[0];
+                    password = lines[1];
+                    server = lines[2];
+                    name = lines[3];
+        }
+        public string get_username() {
+            return username;
+
+        }
+        public string get_password()
+        {
+            return password;
+
+        }
+        public string get_server()
+        {
+            return server;
+
+        }
+        public string get_name()
+        {
+            return name;
+
+        }
+
+        
+    }
+}
