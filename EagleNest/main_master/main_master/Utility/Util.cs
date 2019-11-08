afb6f69705b8c5d03afcbfa2eeec26c25f84eb78
Jeremy <endoffile78@users.noreply.github.com>
2019-11-07 21:31:45 -0800

    Add sha256 function

---
+++ EagleNest/main_master/main_master/Utility/Util.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace main_master.Utility
+{
+    public class Util
+    {
+        public static String sha256(String value)
+        {
+            StringBuilder sb = new StringBuilder();
+            using (SHA256 hash = SHA256Managed.Create())
+            {
+                Encoding enc = Encoding.UTF8;
+                Byte[] result = hash.ComputeHash(enc.GetBytes(value));
+                foreach (Byte b in result)
+                {
+                    sb.Append(b.ToString("x2"));
+                }
+            }
+
+            return sb.ToString();
+        }
+
+    }
+}
