d889989f06f035c422e282880ea8790be5320d0e
endoffile78 <endoffile78@yahoo.com>
2019-11-08 17:12:29 -0600

    Split name into first name and last name and make major and classification not required

--- EagleNest/main_master/main_master/sql/database_build_query.sql
+++ EagleNest/main_master/main_master/sql/database_build_query.sql
@@ -10,9 +10,10 @@ CREATE TABLE [dbo].[User_Main]
 (
  [Email]           nvarchar(50) NOT NULL ,
  [ID_Num]          uniqueidentifier DEFAULT NEWSEQUENTIALID(),
- [Name]            varchar(50) NOT NULL ,
- [Major]           varchar(20) NOT NULL ,
- [Classification]  varchar(15) NOT NULL ,
+ [Fname]           varchar(20) NOT NULL ,
+ [Lnane]           varchar(20) NOT NULL ,
+ [Major]           varchar(20) NULL ,
+ [Classification]  varchar(15) NULL ,
  [User_Type]       tinyint NOT NULL ,
  [Last_Login]      datetime NOT NULL ,
  [Phone]           varchar(15) NULL ,
