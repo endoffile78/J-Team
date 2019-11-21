d466f1e1b55512ccfdc58f2249f9d55d5504da86
Jeremy <endoffile78@users.noreply.github.com>
2019-11-21 16:31:36 -0600

    Start implementing showing who the users follow and who follows them

---
+++ EagleNest/main_master/main_master/Blog/UserProfile.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace main_master.Blog {
+    public class UserProfile {
+        public string name;
+        public Guid uid;
+    }
+}
