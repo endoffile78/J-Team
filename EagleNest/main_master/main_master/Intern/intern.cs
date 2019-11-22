f4a06b708f90d860420e96486c5dd9d3cac01bc2
Jean Marie Uwimana <juwimana81@gmail.com>
2019-11-22 10:39:52 -0600

    Update intern.cs

--- EagleNest/main_master/main_master/Intern/intern.cs
+++ EagleNest/main_master/main_master/Intern/intern.cs
@@ -7,7 +7,6 @@ namespace main_master
 {
     public class intern
     {
-        public string id;
         public string name;
         public string classification;
         public string major;
@@ -17,6 +16,11 @@ namespace main_master
         public string long_desc;
         public string resources;
         public string lessons;
+        public string facebook;
+        public string linkedIn;
+        public string email;
+        public string twitter;
+        public string instagram;
 
     }
 }
