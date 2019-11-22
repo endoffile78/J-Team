aeacf6004008b0b68c778e1ac3edbe9ce8d20354
hecodesfortheusers <jwjoines@eagles.usi.edu>
2019-11-21 20:53:26 -0600

    gave default guid generator to board_post database table

--- EagleNest/main_master/main_master/Board/new_post.aspx.cs
+++ EagleNest/main_master/main_master/Board/new_post.aspx.cs
@@ -13,8 +13,33 @@ namespace main_master.Board
         {
 
            new_post_title.Text = Session["title"].ToString();
-            new_post_description.Text = Session["description"].ToString();
-           
+           new_post_description.Text = Session["description"].ToString();
+            if ((Session["image"].ToString().Length>0)) {
+                new_post_image_label.Text = "Image";
+                new_post_image.ImageUrl = "data:image/png;base64," + Session["image"];
+            
+            }
+
         }
+
+
+        protected void new_post_submit_click(object sender, EventArgs e) { 
+        
+        
+        
+        
+        }
+
+
+        protected void new_post_edit_click(object sender, EventArgs e) { 
+        
+        
+        
+        }
+
+
+
+
+
     }
 }
