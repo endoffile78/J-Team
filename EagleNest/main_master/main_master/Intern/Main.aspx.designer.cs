5aa5727f90aae2a70d5b03164fe47102580f0de0
Jean Marie Uwimana <juwimana81@gmail.com>
2019-11-21 22:59:54 -0600

    Update Main.aspx.designer.cs

--- EagleNest/main_master/main_master/Intern/Main.aspx.designer.cs
+++ EagleNest/main_master/main_master/Intern/Main.aspx.designer.cs
@@ -238,5 +238,14 @@ namespace main_master
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox lessons;
+
+        /// <summary>
+        /// Post control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Post;
     }
 }
